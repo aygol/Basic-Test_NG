04f9ffa1f3fda2df1abb3a4eeb32f19b07cc264b
Agolcu <golcuayse@hotmail.com>
2020-05-04 23:29:48 -0400

    change made it

---
+++ src/test/java/com/automation/tests/homework_4/Clas2.java
@@ -0,0 +1,10 @@
+package com.automation.tests.homework_4;
+
+import com.google.gson.internal.bind.util.ISO8601Utils;
+
+public class Clas2 {
+
+    public static void main(String[] args) {
+        System.out.println("Today is Thusday");
+    }
+}
