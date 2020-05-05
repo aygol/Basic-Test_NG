510f7708c12ca7d20a1f8a4a853af154b39709ae
Agolcu <golcuayse@hotmail.com>
2020-05-04 23:33:05 -0400

    change

--- src/test/java/com/automation/tests/homework_4/Clas1.java
+++ src/test/java/com/automation/tests/homework_4/Clas1.java
@@ -5,5 +5,7 @@ import com.google.gson.internal.bind.util.ISO8601Utils;
 public class Clas1 {
     public static void main(String[] args) {
         System.out.println("Today is monday");
+        System.out.println("Today is monday");
+        System.out.println("Today is monday");
     }
 }
