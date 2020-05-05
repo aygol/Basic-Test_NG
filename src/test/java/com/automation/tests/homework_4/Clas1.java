da426323536206b740c58a1eff2a5fda88b0ff48
Agolcu <golcuayse@hotmail.com>
2020-05-04 23:31:29 -0400

    change theline

--- src/test/java/com/automation/tests/homework_4/Clas1.java
+++ src/test/java/com/automation/tests/homework_4/Clas1.java
@@ -4,6 +4,13 @@ import com.google.gson.internal.bind.util.ISO8601Utils;
 
 public class Clas1 {
     public static void main(String[] args) {
+
         System.out.println("Today is monday");
+        System.out.println("Today is monday");
+        System.out.println("Today is friday");
+
     }
+
+
+
 }
